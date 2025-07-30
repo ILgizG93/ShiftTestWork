385b7a5f0f0abf6583d165cb4bb11de45d9f585f
ADMINion <ilgiz_g93@mail.ru>
2025-07-30 10:30:27 +0300

    tables and migration create

---
+++ src/models.py
@@ -0,0 +1,37 @@
+from datetime import datetime
+from uuid import uuid4
+
+from sqlalchemy import ForeignKey, String, text, UUID
+from sqlalchemy.orm import Mapped, mapped_column, DeclarativeBase, relationship
+
+class ShiftBase(DeclarativeBase):
+    ...
+
+class Users(ShiftBase):
+    __tablename__ = "users"
+    id: Mapped[UUID] = mapped_column(UUID(as_uuid=True), primary_key=True, default=uuid4)
+    login:  Mapped[str] = mapped_column(String(20), index=True)
+    password:  Mapped[str] = mapped_column(String(100))
+    employee_id: Mapped[UUID] = mapped_column(ForeignKey("employees.id"))
+    
+    employee: Mapped["Employees"] = relationship(back_populates="user")
+    token: Mapped["Tokens"] = relationship("Tokens", back_populates="user")
+
+
+class Employees(ShiftBase):
+    __tablename__ = "employees"
+    id: Mapped[UUID] = mapped_column(UUID(as_uuid=True), primary_key=True, default=uuid4)
+    salary: Mapped[int]
+    next_raise_date: Mapped[datetime]
+    
+    user: Mapped["Users"] = relationship(back_populates="employee", uselist=False)
+
+class Tokens(ShiftBase):
+    __tablename__ = "tokens"
+    id: Mapped[UUID] = mapped_column(UUID(as_uuid=True), primary_key=True, default=uuid4)
+    user_id: Mapped[UUID] = mapped_column(ForeignKey("users.id"))
+    token: Mapped[str] = mapped_column(String(500), index=True)
+    expires_at: Mapped[datetime]
+    is_active: Mapped[bool | None]
+    
+    user: Mapped["Users"] = relationship("Users", back_populates="token")
