9b3dc7c4e1376d30d7ab2ede823dee4e64b9e1f3
ADMINion <ilgiz_g93@mail.ru>
2025-07-30 10:29:57 +0300

    settings and database refactoring

--- config/database.py
+++ config/database.py
@@ -1,4 +1,9 @@
 from dataclasses import dataclass
+from typing import AsyncGenerator
+
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
+from sqlalchemy.ext.asyncio.engine import AsyncEngine
 
 @dataclass
 class Server:
@@ -7,15 +12,30 @@ class Server:
     username: str
     password: str
 
-    def __init__(self, host, port, username, password) -> None:
-        self.host = host
-        self.port = port
-        self.username = username
-        self.password = password
+    def __init__(self, *args) -> None:
+        self.host = args[0]
+        self.port = args[1]
+        self.username = args[2]
+        self.password = args[3]
 
 
 @dataclass
 class DataBaseServer(Server):
-    def __init__(self, host, port, username, password, name) -> None:
-        super().__init__(host, port, username, password)
-        self.name: str = name
+    name: str
+    _url_async: str
+
+    def __init__(self, *args) -> None:
+        super().__init__(args[0], args[1], args[2], args[3])
+        self.name = args[4]
+        self._url_async = f"postgresql+asyncpg://{self.username}:{self.password}@{self.host}:{self.port}/{self.name}"
+        # self._engine_async: AsyncEngine = create_async_engine(url=self._url_async, echo=False)
+        # self.session_async = async_sessionmaker(self._engine_async, class_=AsyncSession, expire_on_commit=False)
+
+    async def get_session_async(self) -> AsyncGenerator[AsyncSession, None]:
+        try:
+            air_engine: AsyncEngine = create_async_engine(self._url_async)
+            air_async_session = sessionmaker(air_engine, class_=AsyncSession, expire_on_commit=False)
+            session: AsyncSession = air_async_session()
+            yield session
+        finally:
+            await session.close()
