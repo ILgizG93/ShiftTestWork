53ebdf9623bf7bc53cb284e9292eb9dcbbc7a1d4
ADMINion <ilgiz_g93@mail.ru>
2025-08-02 15:09:13 +0300

    refactoring

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 from fastapi import FastAPI
 
 from config.settings import Settings
-from loggers import init_logger
 from src.routers import router
 
 app = FastAPI()
 app.include_router(router)
-logger = init_logger("app")
 settings: Settings = Settings()
